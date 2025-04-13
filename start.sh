1e86125bf86d34214a835e7f372b7c732f8788ba
Rinku-ux <66407137+Rinku-ux@users.noreply.github.com>
2025-04-13 17:41:25 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 set -e
 
-# 必要なものをインストール
-apt update
-apt install -y chromium chromium-driver
+# Chromiumインストール用の最低限パッケージ
+apt-get update
+apt-get install -y wget gnupg2
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+apt-get update
+apt-get install -y google-chrome-stable
 
-# パスを確認（オプション）
-which chromium
-which chromium-browser
+# パス確認
+which google-chrome-stable
 
-# スクリプトを実行
+# Pythonスクリプト実行
 python wowow_schedule.py
