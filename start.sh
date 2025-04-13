274375f4c84ee24b4eb06f67c87a01c9a3dfab25
Rinku-ux <66407137+Rinku-ux@users.noreply.github.com>
2025-04-13 17:44:39 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-# Chromiumインストール用の最低限パッケージ
+# 必要なツールをインストール
 apt-get update
 apt-get install -y wget gnupg2
+
+# Googleの公開鍵を登録
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+
+# Google Chromeリポジトリを追加
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+
+# パッケージ情報更新＆Google Chromeインストール
 apt-get update
 apt-get install -y google-chrome-stable
 
-# パス確認
+# インストールしたパスを確認（任意）
 which google-chrome-stable
 
-# Pythonスクリプト実行
+# スクリプト実行
 python wowow_schedule.py
