97ebeba1b239248dd5c113d04d562d13e568196b
Rinku-ux <66407137+Rinku-ux@users.noreply.github.com>
2025-04-13 17:37:54 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-# Chromium をインストール
+# 必要なものをインストール
 apt update
-apt install -y chromium-browser
+apt install -y chromium chromium-driver
+
+# パスを確認（オプション）
+which chromium
+which chromium-browser
 
 # スクリプトを実行
 python wowow_schedule.py
